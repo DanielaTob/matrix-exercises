8479bdba317b98bccb65bfa821bff88e30774d97
daniela <moreno.tobar.daniela@gmail.com>
2023-03-13 15:24:48 -0500

    first commit

---
+++ src/main/java/com/project/matrix/Main.java
@@ -0,0 +1,25 @@
+package com.project.matrix;
+
+public class Main {
+
+        public static void main(String[] args) {
+
+            int filas = 4;
+            int columnas = 4;
+
+
+            int [][] matriz = new int[filas][columnas];
+
+            for (int i = 1; i < filas; i++){
+                for (int j = columnas -1; j > 0; j--){
+                    if (j == i){
+                        System.out.print(1);
+                    }else {
+                        System.out.print(" - ");
+                    }
+                }
+                System.out.println();
+            }
+        }
+
+}
